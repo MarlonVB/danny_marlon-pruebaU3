6d6c87aba146e5a35d20eea92502a08b004626a0
Danny Tenemea <danny.tenemea.est@tecazuay.edu.ec>
2022-09-06 07:50:42 -0500

    creacion de modelos

--- src/main/java/ec/edu/ista/marlondanny/danny_marlonpruebaU3/modelo/Proyecto.java
+++ src/main/java/ec/edu/ista/marlondanny/danny_marlonpruebaU3/modelo/Proyecto.java
@@ -1,4 +1,20 @@
 package ec.edu.ista.marlondanny.danny_marlonpruebaU3.modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
 public class Proyecto {
+
+    @Id
+    private Long id;
+    private String referencia;
+    private String nombre_del_proyecto;
+    private String objetivo_estrategico;
+    private String objetivo_tactico;
+    private String estrategia;
+    private String objetivo_operativo;
+
 }
---
+++ src/main/java/ec/edu/ista/marlondanny/danny_marlonpruebaU3/modelo/Seguimiento.java
@@ -0,0 +1,25 @@
+package ec.edu.ista.marlondanny.danny_marlonpruebaU3.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+public class Seguimiento {
+    @Id
+    private Long id;
+    private String referencia;
+    private String prioridad;
+    private String fecha_entrega;
+    private String tarea;
+    private String docente_responsable;
+    private String descripcion;
+    private String entregable;
+    private int ejecutado;
+    private String grupo;
+    private int costo_fijo;
+    private int horas_estimadas;
+    private int horas_actuales;
+    private String observacion;
+}
